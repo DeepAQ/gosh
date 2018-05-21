50dbfd6e6a57437ad8c374787d0259a12936887b
Monkey Liang <i@aq741.com>
2018-05-21 20:31:37 +0800

    fix startup script

--- start-agent.sh
+++ start-agent.sh
@@ -8,7 +8,7 @@ echo ETCD_URL = $ETCD_URL
 
 if [[ "$1" == "consumer" ]]; then
   echo "Starting consumer agent..."
-  gosh -type=consumer -port=20000 -etcd=http://127.0.0.1:2379
+  gosh -type=consumer -port=20000 -etcd=$ETCD_URL
 elif [[ "$1" == "provider-small" ]]; then
   echo "Starting small provider agent..."
   gosh -type=provider -port=30000 -dubbo.port=20880 -etcd=$ETCD_URL
